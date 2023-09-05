83719c524affd8d6ed5e966e56bab00dd9e60094
Júlia <juliadecastro777@gmail.com>
2023-09-05 09:59:24 -0300

    Inserindo arquivo para instalação das bibliotecas

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
-from flasgger import Swagger
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://monitoradb:3YZFEp5Z6GJpA6BNGlTqDDPNGma8BInf@dpg-cis2uvdph6et1sd86n8g-a.oregon-postgres.render.com/monitoradb'
 db = SQLAlchemy(app)
 CORS(app)
-swagger = Swagger(app)
 
 from app import routes
